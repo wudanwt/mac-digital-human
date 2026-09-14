cfb30f08f15c9250515ac78190a3c970794b739f
wudanwt <wudanwt@163.com>
2026-09-14 16:46:48 +0800

    refactor: expose multi-provider TTS API

--- app/tts/__init__.py
+++ app/tts/__init__.py
@@ -1,3 +1,15 @@
-from .mlx_audio import MLXAudioTTS, TTSConfig, TTSError
+from .audio8_onnx import Audio8Config, Audio8ONNXTTS
+from .base import TTSError, TTSProvider
+from .mlx_audio import MLXAudioTTS, TTSConfig
+from .registry import create_tts, infer_provider
 
-__all__ = ["MLXAudioTTS", "TTSConfig", "TTSError"]
+__all__ = [
+    "Audio8Config",
+    "Audio8ONNXTTS",
+    "MLXAudioTTS",
+    "TTSConfig",
+    "TTSError",
+    "TTSProvider",
+    "create_tts",
+    "infer_provider",
+]
