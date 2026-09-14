22c5437726b9e95c453e0c4ea7d4c3185f74ed30
wudanwt <wudanwt@163.com>
2026-09-14 09:06:54 +0800

    refactor: expose avatar engine registry

---
+++ app/engines/__init__.py
@@ -0,0 +1,10 @@
+from .base import EngineError, RenderResult
+from .longcat import LongCatMLXEngine
+from .musetalk import MuseTalkMLXEngine
+
+__all__ = [
+    "EngineError",
+    "RenderResult",
+    "MuseTalkMLXEngine",
+    "LongCatMLXEngine",
+]
