63bbfdb917073a6f4abc5c7686997e582e76ca91
wudanwt <wudanwt@163.com>
2026-09-20 16:13:38 +0800

    Export MuseTalk backend factory

--- app/engines/__init__.py
+++ app/engines/__init__.py
@@ -1,8 +1,11 @@
 from .base import EngineError, RenderResult
 from .musetalk import MuseTalkMLXEngine
+from .factory import create_musetalk_engine, normalize_musetalk_backend
 
 __all__ = [
     "EngineError",
     "RenderResult",
     "MuseTalkMLXEngine",
+    "create_musetalk_engine",
+    "normalize_musetalk_backend",
 ]
